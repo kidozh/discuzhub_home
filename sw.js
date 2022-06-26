551cd2015ffd9fd3844a5bf7829b4c3aa7f5a092
kidozh <331837926@qq.com>
2022-06-26 02:40:04 +0100

    Updates

--- sw.js
+++ sw.js
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "styles-22d42ec26f6458da241b.js"
   },
   {
-    "url": "app-cb2c953437bd87a78ed0.js"
+    "url": "app-84092886109ce7a05056.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2c29fdcf4181314808c55ef64dbcf21b"
+    "revision": "d0394bf611b95ba5d35b1b34d134445a"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-999e66766b99e2071776.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "c172399bdba90ea7d6b93a8d79c43dd2"
+    "revision": "175f5930029e23c60e4a5c599d2e91de"
   },
   {
     "url": "polyfill-70079845db5d7657d6ba.js"
@@ -153,7 +153,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-cb2c953437bd87a78ed0.js`))) {
+  if (!resources || !(await caches.match(`/app-84092886109ce7a05056.js`))) {
     return await fetch(event.request)
   }
 
